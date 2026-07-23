192e556c5306a4e0e833838299152df784b5f01b
mazharfareed976-collab <mazharfareed976@gmail.com>
2026-07-23 20:30:28 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,29 @@
+const website = {
 
+    title: "e-Visa Portal",
+
+    logo: {
+        image: "images/logo.png",
+        alt: "e-Visa Logo"
+    },
+
+    language: {
+        default: "EN",
+        available: ["EN", "AL"]
+    },
+
+    headerButtons: {
+
+        notification: {
+            text: "🔔",
+            link: "pages/notifications.html"
+        },
+
+        login: {
+            text: "👤",
+            link: "pages/login.html"
+        }
+
+    }
+
+};
