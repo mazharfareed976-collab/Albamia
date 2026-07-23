d086033cfa2e15c613ff5e1e8e9a05dcce2f3503
mazharfareed976-collab <mazharfareed976@gmail.com>
2026-07-23 20:31:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+const header = document.getElementById("header");
 
+header.innerHTML = `
+
+<div class="top-header">
+
+    <div class="logo">
+
+        <a href="index.html">
+            <img src="${website.logo.image}" alt="${website.logo.alt}">
+        </a>
+
+    </div>
+
+    <div class="right-menu">
+
+        <a href="${website.headerButtons.notification.link}" class="icon">
+            ${website.headerButtons.notification.text}
+        </a>
+
+        <a href="${website.headerButtons.login.link}" class="icon">
+            ${website.headerButtons.login.text}
+        </a>
+
+        <div class="language">
+
+            <button>EN</button>
+            <button>AL</button>
+
+        </div>
+
+    </div>
+
+</div>
+
+`;
